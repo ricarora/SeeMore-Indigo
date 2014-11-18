ac7ecebe616c43c34d64fe991f9438a91bf25d0b
Cate Uselton <cathode.use@gmail.com>
2014-11-18 14:12:03 -0800

    added current_bro helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  helper :all
   protect_from_forgery with: :exception
+
+  private
+
+  def current_bro
+    @current_bro ||= User.find_by(id: session[:bro_id])
+  end
+  helper_method :current_bro
+
 end
