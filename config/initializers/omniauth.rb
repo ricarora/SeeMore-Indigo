ee693b4715bb8faf94e9d288366585f28573ac5a
Kat <kmmpatke@gmail.com>
2014-11-18 10:43:52 -0800

    added omniauth.rb file

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+use OmniAuth::Builder do
+  provider :developer,
+    :fields => [:first_name, :last_name],
+    :uid_field => :last_name
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118182712) do
+ActiveRecord::Schema.define(version: 20141118183824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "authentications", force: true do |t|
     t.string   "provider"
-    t.integer  "uid"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "uid"
   end
 
   create_table "users", force: true do |t|
