caf97cd3043bcd5220cb43278f6dfb88b4ef901a
Cate Uselton <cathode.use@gmail.com>
2014-11-18 14:07:38 -0800

    added user and authentication create to sessions#create

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,16 +1,18 @@
 class SessionController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: :create
 
+  #TODO: create a new user and authentication not in sessionContoller
+
   def create
     login = Authentication.where(uid: request.env["omniauth.auth"][:uid], provider: request.env["omniauth.auth"][:provider])
     if login.empty?
+      User.create(name: request.env["omniauth.auth"][:first_name], email: request.env["omniauth.auth"][:last_name])
+      login << User.authentications.create(provider: request.env["omniauth.auth"][:provider], uid: request.env["omniauth.auth"][:uid])
       # If empty, create a new Authentication/User
-      # Authenication.request.env["omniauth.auth"][:uid]
-    else
-      session[:bro_id] = login.first.user.id
-      User.create
     end
-
+    session[:bro_id] = login.first.user.id
+    redirect_to root_path, notice: "Mi casa es tu casa, brah"
   end
+
 
 end
