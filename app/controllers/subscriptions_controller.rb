13fcaf500d3ae705cfc123ea69f4f00d35d55008
Cate Uselton <cathode.use@gmail.com>
2014-11-19 13:19:20 -0800

    find or create method for subscription user relationship in subscription controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,5 +1,19 @@
 class SubscriptionsController < ApplicationController
+
   def create
+    find_or_create(params[:subscription])
     raise
   end
+
+  private
+
+  def find_or_create(subs_hash)
+    subscrip = Subscription.where(uid: subs_hash[:uid], provider: subs_hash[:provider] )
+    if subscrip.empty?
+      subscrip = Subscription.create(uid: subs_hash[:uid], provider: subs_hash[:provider])
+    end
+    current_bro.subscriptions << subscrip
+  end
+
+
 end
