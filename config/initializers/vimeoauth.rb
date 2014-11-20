095d661f27b29674feb4c5aa305a027231058c89
Kat <kmmpatke@gmail.com>
2014-11-20 09:42:47 -0800

    testing out

--- config/initializers/vimeoauth.rb
+++ config/initializers/vimeoauth.rb
@@ -2,4 +2,4 @@ require 'vimeo'
 base = Vimeo::Advanced::Base.new(ENV["VIMEO_CLIENT_IDENTIFIER"], ENV["VIMEO_CLIENT_SECRET"])
 request_token = base.get_request_token
 SECRET = request_token.secret
-$person = Vimeo::Advanced::Person.new(ENV["VIMEO_CLIENT_IDENTIFIER"], ENV["VIMEO_CLIENT_SECRET"], :token => ENV["VIMEO_ACCESS_TOKEN"], :secret => SECRET)
+$video = Vimeo::Advanced::Video.new(ENV["VIMEO_CLIENT_IDENTIFIER"], ENV["VIMEO_CLIENT_SECRET"], :token => ENV["VIMEO_ACCESS_TOKEN"], :secret => SECRET)
