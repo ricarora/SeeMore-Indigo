92bab6f87f2c2221414e74f65addfdceb83fc7f9
Richa Arora <ricarora@gmail.com>
2014-11-20 14:07:42 -0800

    edit/update user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
   def show
   end
 
+  def update
+    current_bro.update(params.require(:user).permit(:name, :email))
+    redirect_to show_path
+  end
+
 end
