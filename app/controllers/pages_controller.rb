17fbdbe06f2c2af9358f16f417f3ad394e1ea3a2
Bri Dotson <bridotson@gmail.com>
2014-11-19 16:07:35 -0800

    update load feed to fill an array with hashes of feed items, add add_tweets_to_feed method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,14 +14,30 @@ class PagesController < ApplicationController
 
   #possibly this should be moved to another controller or in a model (user)???
   def load_feed
-    @tweets = []
+    # each item in the feed is a hash that has this:
+    # provider_image_url, avatar, username, created_at, content
+    @feed = []
     current_bro.subscriptions.each do |subs|
-      #later we'll need to check the provider first
-      @tweets += $client.user_timeline(subs.uid.to_i)
+      case subs.provider
+      when "twitter"
+        tweets = $client.user_timeline(subs.uid.to_i)
+        add_tweets_to_feed(tweets)
+      #when "vimeo"
+      end
     end
   end
 
-  #provider image, user image, username, content
+  def add_tweets_to_feed(tweets)
+    tweets.each do |tweet|
+      @feed << {
+        provider_image_url: "twitter-64-black.png",
+        avatar: tweet.user.profile_image_url.to_s,
+        username: tweet.user.username,
+        created_at: tweet.created_at,
+        content: tweet.full_text
+        }
+    end
+  end
 
   def twitter_user_search
     @subscription = Subscription.new
