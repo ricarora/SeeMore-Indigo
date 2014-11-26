8263342245641f855137959991e4264a77c71709
Cate Uselton <cathode.use@gmail.com>
2014-11-26 13:34:56 -0800

    unsubscribe msgs method

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,11 @@
 class Subscription < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :feed_items
+
+  def unsubscribe_msg
+    msgs = ["Break it off", "End it", "Make a clean break", "Go no contact",
+       "Slow fade", "Hit the gym", "It's you, not me"]
+    msgs.sample
+  end
+
 end
