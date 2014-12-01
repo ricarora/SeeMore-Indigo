271bdc3a72f83a728312fa4827dde44626e48aa2
Cate Uselton <cathode.use@gmail.com>
2014-12-01 11:32:09 -0800

    private accounts cannot be added when javascript is turned off

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -41,26 +41,26 @@ $(function() {
 
 
 //  This will subscribe the result on a search page
-  $(".search_result").click(function(e) {
-    e.preventDefault();
-    var current = $(this);
-    var current_hidden = $(current).siblings(".unsearch_result");
-    // var value = $(this).siblings("div").children()[1];
-    var $form = $(this).parents("form");
-    $.ajax({
-      url: "/subscriptions",
-      type: "POST",
-      data: $form.serialize(),
-      // dataType: "script",
-      success: function() {
-        $(current_hidden).removeClass("hide");
-        $(current).addClass("hide");
-      },
-      error: function(err) {
-        alert("Error!!");
-      },
-    });
-  });
+  // $(".search_result").click(function(e) {
+  //   e.preventDefault();
+  //   var current = $(this);
+  //   var current_hidden = $(current).siblings(".unsearch_result");
+  //   // var value = $(this).siblings("div").children()[1];
+  //   var $form = $(this).parents("form");
+  //   $.ajax({
+  //     url: "/subscriptions",
+  //     type: "POST",
+  //     data: $form.serialize(),
+  //     // dataType: "script",
+  //     success: function() {
+  //       $(current_hidden).removeClass("hide");
+  //       $(current).addClass("hide");
+  //     },
+  //     error: function(err) {
+  //       alert("Error!!");
+  //     },
+  //   });
+  // });
 
 
 //  This will unsubscribe the result on a search page
