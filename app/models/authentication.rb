6f9615df229dbb77c0ea57d7db445948ee8bd8ad
Cate Uselton <cathode.use@gmail.com>
2014-11-30 17:36:30 -0800

    able to add instagram authentication

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -2,3 +2,13 @@ class Authentication < ActiveRecord::Base
   belongs_to :user
 
 end
+
+
+
+# you visit the auth callback page
+# it checks if you're logged in
+# if you are, it tries to find auth
+# if auth exists, tells you that auth is already associate with another user
+# if not, it creates that auth and adds it to this bro
+
+# if not logged in, tries to find auth, if found returns user, if not creates user
