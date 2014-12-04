f68c2c7ea47437be215d8ac09d7e3c7550df8644
Cate Uselton <cathode.use@gmail.com>
2014-12-04 09:14:24 -0800

    Update users_controller_spec.rb
    Adds a reload for test to perform correctly

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,12 +4,11 @@ describe UsersController, :type => :controller do
   describe '#update' do
     before {User.create(name: "meow", email: "cat@meow.com")}
     before { session[:bro_id] = User.last.id }
-    # Why does this test fail? Unknown. Mystery.
-    xit "updates user name and email from params" do
+    it "updates user name and email from params" do
       bro = User.find(session[:bro_id])
       patch :update, {id: bro.id, user: {name: "whiskers"} }
       # puts bro.inspect + "%%%%%%%%%%%%%"
-      expect(bro.name).to eq "whiskers"
+      expect(bro.reload.name).to eq "whiskers"
     end
   end
 end
